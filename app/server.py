44b2733ac5a2552c24d4c2591329b7b5364051f6
David <david.nunez.dev@gmail.com>
2021-03-10 22:36:55 -0500

    add new endpoint

--- app/server.py
+++ app/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/DevOps')
+def devops():
+    return "Esto es un nuevo endpoint"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
