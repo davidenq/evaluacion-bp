37e7403d4d8e5cbf13dc74117b5276c5a7a01db2
David <david.nunez.dev@gmail.com>
2021-03-12 23:01:55 -0500

    update to pass env var from yml to bash

--- scripts/k8s/deployment.sh
+++ scripts/k8s/deployment.sh
@@ -6,7 +6,7 @@ kubectl apply -f ./infra/deployments/app/configmap.yml
 # create deployment
 DEVPATH=./infra/deployments/app/deployment.yml
 
-awk -v envhash=${github.sha} '{gsub(/HASH/, envhash);print}' $DEVPATH >> temp.yml
+awk -v envhash=$SHA '{gsub(/HASH/, envhash);print}' $DEVPATH >> temp.yml
 kubectl apply -f ./temp.yml
 rm -rf ./temp.yml
 
